b7e622b0e5d6cdbe96c97ee41e643567b66a3eaa
Paul Vidal <78744667+pvidal-vmc@users.noreply.github.com>
2022-07-01 10:33:28 -0500

    Update ThrowExceptionOnFailurePlugin.php

--- src/Postmark/ThrowExceptionOnFailurePlugin.php
+++ src/Postmark/ThrowExceptionOnFailurePlugin.php
@@ -2,12 +2,19 @@
 
 namespace Postmark;
 
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Str;
+
 class ThrowExceptionOnFailurePlugin implements \Swift_Events_ResponseListener
 {
     public function responseReceived(\Swift_Events_ResponseEvent $event)
     {
         if (!$event->isValid()) {
-            throw new \Swift_TransportException($event->getResponse());
+            if (Str::contains($event->getResponse(), 'ErrorCode":406')) {
+                Log::critical($event->getResponse());
+            } else {
+                throw new \Swift_TransportException($event->getResponse());
+            }
         }
     }
 }
